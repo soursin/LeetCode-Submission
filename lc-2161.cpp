2503857974d844e673a9f10e6cbab35d3eb5ded0
Sourabh Singh <imhashvoid8@gmail.com>
2025-03-03 09:46:19 +0530

    Two pointer approach is the best

--- lc-2161.cpp
+++ lc-2161.cpp
@@ -24,3 +24,27 @@ class Solution {
             return nums;
         }
     };
+
+//Better Approach 
+/*
+
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> res(nums.size(),pivot);
+        int left = 0 ,right = nums.size()-1;
+        for(int i=0,j=nums.size()-1; i<nums.size();i++,j--){
+            if(nums[i] < pivot){
+                res[left] = nums[i];
+                left++;
+            }
+            if(nums[j] > pivot){
+                res[right] = nums[j];
+                right--;
+            }
+        }
+        return res;
+    }
+};
+
+*/
