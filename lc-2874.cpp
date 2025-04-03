e5051b2057d47fc5a512f123d9f1e1f1b3fd6907
Sourabh Singh <imhashvoid8@gmail.com>
2025-04-03 09:28:35 +0530

    Solved by 0(n) approach

---
+++ lc-2874.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public:
+        long long maximumTripletValue(vector<int>& nums) {
+            long maxTriplet = 0 , maxElement = 0 , maxDiff = 0;
+            for(long num : nums){
+                maxTriplet = max(maxTriplet,maxDiff*num);
+                maxDiff = max(maxDiff,maxElement - num);
+                maxElement = max(maxElement,num);
+            }
+    
+            return maxTriplet < 0 ? 0 : maxTriplet;
+        }
+    };
